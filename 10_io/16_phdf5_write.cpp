4e5b5aa6a9132c5ba8d387fe802ed5d95799d2c3
syamoji141 <syamoji141@gmail.com>
2022-05-23 07:35:13 +0900

    Homework_10

--- 10_io/16_phdf5_write.cpp
+++ 10_io/16_phdf5_write.cpp
@@ -17,8 +17,10 @@ int main (int argc, char** argv) {
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
   for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  // hsize_t count[2] = {1,1};
+  hsize_t count[2] = {2,2};
+  // hsize_t stride[2] = {1,1};
+  hsize_t stride[2] = {NX/dim[0], NY/dim[1]};
   vector<int> buffer(Nlocal[0]*Nlocal[1],mpirank);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
